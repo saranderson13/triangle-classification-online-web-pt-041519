6970623d87edaac91127bffe2d84f8c85e02f027
github username <github email address>
2019-05-26 14:27:49 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_accessor :s1, :s2, :s3, :kind
+
+  def initialize(s1, s2, s3)
+    @s1, @s2, @s3 = s1, s2, s3
+    @kind = triangle_test
+  end
+
+  def triangle_test
+    if valid?
+      if s1 == s2
+        s1 == s3 ? @kind = :equilateral : @kind = :isosceles
+      elsif s1 == s3 || s2 == s3
+        @kind = :isosceles
+      else @kind = :scalene
+      end
+    else raise TriangleError
+    end
+  end
+
+  def valid?
+    side_array = s1, s2, s3; side_array.sort!
+    side_array.all? {|x| x > 0 } && side_array[0] + side_array[1] > side_array[2]
+  end
+
+  class TriangleError < StandardError
+  end
 end
